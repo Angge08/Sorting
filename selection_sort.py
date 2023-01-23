b8bf8230c7db64df2cfc8ff91c9707ce11086e8b
Angelica Delmendo <anggedelmendo08@gmail.com>
2023-01-24 02:07:33 +0800

    putting the condition for i and j in range

--- selection_sort.py
+++ selection_sort.py
@@ -1,1 +1,15 @@
-try
+def selection_sort(arr):
+
+    for i in range (0, len(arr) - 1):
+        minpos = i
+        for j in range (i + 1, len(arr)):
+            if arr[j] < arr[minpos]:
+                minpos = j
+
+        arr[i], arr[minpos] = arr[minpos], arr[i]
+
+        print(arr)
+
+arr = [18, 100, 70, 64, 19, 90, 93, 8, 33, 92]
+selection_sort(arr)
+print(arr)
