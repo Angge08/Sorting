2afc31747b8aab68c27cd78625b53e7e6264bde2
Angelica Delmendo <anggedelmendo08@gmail.com>
2023-01-24 02:34:21 +0800

    selection_sort

---
+++ bubble_sort.py
@@ -0,0 +1,13 @@
+def bubble_sort(arr):
+    for i in range(len(arr) -1, 0, -1):
+        for j in range (i):
+            if arr[j] > arr[j+1]:
+                temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
+
+        print(arr)
+
+arr = [18, 100, 70, 64, 19, 90, 93, 8, 33, 92]
+bubble_sort(arr)
+print(arr)
